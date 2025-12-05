f7753ce0883598795f45778b94528512d99bebf7
Martin Stendorf <mstendorf@gmail.com>
2025-12-05 09:28:20 +0100

    adding 2025 day 5

---
+++ 2025/day5/main.py
@@ -0,0 +1,51 @@
+f = open("input.txt", "r")
+
+lines = f.readlines()
+
+ranges = []
+items = []
+
+ranges_part = True
+
+for line in lines:
+    if line.strip() == "":
+        ranges_part = False
+        continue
+
+    if ranges_part:
+        parts = line.strip().split("-")
+        low, high = map(int, parts)
+        ranges.append((low, high))
+    else:
+        num = int(line.strip())
+        items.append(num)
+
+
+def part1(ranges, items):
+    sum = 0
+    for item in items:
+        for r in ranges:
+            if r[0] <= item <= r[1]:
+                sum += 1
+                break
+
+    print("Part 1:", sum)
+
+
+def part2(ranges):
+    merged_ranges = []
+    for r in sorted(ranges):
+        if not merged_ranges or merged_ranges[-1][1] < r[0] - 1:
+            merged_ranges.append(r)
+        else:
+            merged_ranges[-1] = (merged_ranges[-1][0], max(merged_ranges[-1][1], r[1]))
+
+    sum = 0
+    for r in merged_ranges:
+        sum += r[1] - r[0] + 1
+
+    print("Part 2:", sum)
+
+
+part1(ranges, items)
+part2(ranges)
